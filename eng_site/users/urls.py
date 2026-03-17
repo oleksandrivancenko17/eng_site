0f454bb7ff9d4c515265921f3a5674aeb6004d7e
oleksandrivancenko17 <oleksandrivancenko36@gmail.com>
2026-03-17 22:57:08 +0200

    feat: add styled login and register templates

--- eng_site/users/urls.py
+++ eng_site/users/urls.py
@@ -5,7 +5,7 @@ from users import views
 app_name = 'users'
 
 urlpatterns = [
-    path('login/', views.Login_View.as_view, name='login'),
-    path('register/', views.Register_View.as_view, name='register'),
-    path('logout/', views.Logout_View.as_view, name='logout'),
+    path('login/', views.UserLoginView.as_view(), name='login'),
+    path('register/', views.UserRegisterView.as_view(), name='register'),
+    path('logout/', views.UserLogoutView.as_view(), name='logout'),
 ]
--- eng_site/users/views.py
+++ eng_site/users/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from django.contrib.auth.views import LoginView, LogoutView
+from django.views.generic import CreateView
+from django.urls import reverse_lazy
+from django.contrib.auth import login
+from .forms import CustomUserCreationForm
 
-# Create your views here.
+
+class UserLoginView(LoginView):
+    template_name = 'login.html'
+    redirect_authenticated_user = True
+
+    def get_success_url(self):
+        return reverse_lazy('dashboard')
+
+
+class UserLogoutView(LogoutView):
+    next_page = reverse_lazy('users:login')
+
+
+class UserRegisterView(CreateView):
+    form_class = CustomUserCreationForm
+    template_name = 'register.html'
+    success_url = reverse_lazy('dashboard')
+
+    def form_valid(self, form):
+        response = super().form_valid(form)
+        login(self.request, self.object)
+        return response
