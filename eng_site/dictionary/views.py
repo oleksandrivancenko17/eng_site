d64d3b2846301d58b9fb0aecce991affbe9d6532
oleksandrivancenko17 <oleksandrivancenko36@gmail.com>
2026-03-19 00:11:59 +0200

    feat: add search, level, and dynamic category filters to dictionary

--- eng_site/dictionary/views.py
+++ eng_site/dictionary/views.py
@@ -1,3 +1,4 @@
+from django.db.models import Q
 from django.shortcuts import render
 from django.views.generic import ListView
 from dictionary.models import Category, Word
@@ -10,4 +11,34 @@ class DictionaryListView(ListView):
     paginate_by = 10
 
     def get_queryset(self):
-        return Word.objects.select_related('category').all()
+        queryset = Word.objects.select_related('category').all()
+
+        search_query = self.request.GET.get('q')
+
+        level_query = self.request.GET.get('level')
+
+        category_query = self.request.GET.get('category')
+
+        if search_query:
+            queryset = queryset.filter(
+                Q(english_word__icontains=search_query) |
+                Q(translation__icontains=search_query)
+            )
+
+
+        if level_query and level_query != 'all':
+            queryset = queryset.filter(
+                Q(level__startswith=level_query)
+            )
+
+
+        if category_query and category_query != 'all':
+            queryset = queryset.filter(category_id = category_query)
+
+
+        return queryset
+
+    def get_context_data(self, **kwargs):
+        context = super(DictionaryListView, self).get_context_data(**kwargs)
+        context['categories'] = Category.objects.all()
+        return context
